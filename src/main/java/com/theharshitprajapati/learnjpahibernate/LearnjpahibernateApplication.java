9d41cd662f916f22c6ea109fa299e797f9e9eb7e
Harshit Prajapati <harshitprajapati7666@gmail.com>
2023-02-23 17:47:15 +0530

    Till Step 6.3

---
+++ src/main/java/com/theharshitprajapati/learnjpahibernate/LearnjpahibernateApplication.java
@@ -0,0 +1,13 @@
+package com.theharshitprajapati.learnjpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnjpahibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnjpahibernateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/theharshitprajapati/learnjpahibernate/LearnjpahibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theharshitprajapati.learnjpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnjpahibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
